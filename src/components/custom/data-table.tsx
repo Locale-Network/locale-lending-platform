c582c0313930c98b3dfc9bed8f735c556957f62b
sajee_techi <sajeewa@techi.be>
2024-11-11 00:24:34 +0530

    reusable components

---
+++ src/components/custom/data-table.tsx
@@ -0,0 +1,28 @@
+import { DataTable } from '@/components/ui/data-table';
+import Pagination from '@/components/custom/pagination';
+import { ColumnDef } from '@tanstack/react-table';
+
+export default async function Table<T>({
+  rows = [],
+  columns = [],
+  total = 0,
+  totalPages = 0,
+}: {
+  rows: T[];
+  columns: ColumnDef<T>[];
+  total: number;
+  totalPages: number;
+}) {
+
+  return (
+    <div className="flex flex-1 flex-col gap-4 overflow-auto">
+      <DataTable columns={columns} data={rows} />
+      <div className="flex w-full items-center justify-center">
+        <p className="flex flex-row gap-2 whitespace-nowrap">
+          Total: {total}
+        </p>
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
+}
---
+++ src/components/custom/url-search.tsx
@@ -0,0 +1,39 @@
+'use client';
+import { useCallback } from 'react';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { Input } from '@/components/ui/input';
+import { debounce } from 'lodash';
+
+export default function Search() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  // Create the debounced version of handleSearch
+  const debouncedSearch = useCallback(
+    debounce((term: string) => {
+      const params = new URLSearchParams(searchParams);
+      params.set('page', '1');
+      if (term) {
+        params.set('query', term);
+      } else {
+        params.delete('query');
+      }
+      replace(`${pathname}?${params.toString()}`);
+    }, 300),
+    [searchParams, pathname, replace]
+  );
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    debouncedSearch(event.target.value);
+  }
+
+  return (
+    <Input
+      placeholder="Search"
+      onChange={handleChange}
+      className="max-w-sm"
+      defaultValue={searchParams.get('query')?.toString()}
+    />
+  );
+}
