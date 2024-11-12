3afdd74480cccb9c45a2201f380fc0a2467ee09c
Thidasa Paranavitharana <thidasa.pankaja@gmail.com>
2024-11-12 23:44:41 +0800

    fixed a function rename

--- src/components/KycFeature.tsx
+++ src/components/KycFeature.tsx
@@ -4,11 +4,11 @@ import useKycVerification from "@/hooks/useKycVerification";
 import { Button } from "./ui/button";
 
 const IdentityVerification: React.FC<{ clientUserId?: string }> = () => {
-  const { openKycLink, linkToken, } = useKycVerification();
+  const { startKYCFlow, linkToken, } = useKycVerification();
 
   return (
     <div>
-      <Button disabled={!linkToken} onClick={openKycLink}>
+      <Button disabled={!linkToken} onClick={startKYCFlow}>
         Verify KYC
       </Button>
     </div>
