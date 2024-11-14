41daaa66a82fdc454cfdb83841f01ec88e4ee19e
sajee_techi <sajeewa@techi.be>
2024-11-14 17:00:55 +0530

    use authOptions with server session

--- src/app/signin/actions.ts
+++ src/app/signin/actions.ts
@@ -2,12 +2,13 @@
 import { isAddress } from 'viem';
 
 import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/auth-options';
 import { upsertChainAccount } from '@/services/db/chain-accounts';
 import { ChainAccount } from '@prisma/client';
 
 export async function signIn(address: string): Promise<ChainAccount | null> {
   try {
-    const session = await getServerSession();
+    const session = await getServerSession(authOptions);
 
     if (!session || !isAddress(address)) {
       return null;
