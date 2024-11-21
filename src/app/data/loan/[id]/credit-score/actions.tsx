e955fd34f821b6ba4266edf20f317cdf1af1ae80
sajee_techi <sajeewa@techi.be>
2024-11-21 23:41:24 +0530

    initialise PlaidLink session for reclaim

--- src/app/data/loan/[id]/credit-score/actions.tsx
+++ src/app/data/loan/[id]/credit-score/actions.tsx
@@ -1,45 +1,96 @@
 'use server';
 
-import {
-  getLoanApplication as dbGetLoanApplication,
-  saveAccessTokenOfLoanApplicationCreator as dbSaveAccessTokenOfLoanApplicationCreator,
-} from '@/services/db/loan-applications';
+import { getLoanApplication as dbGetLoanApplication } from '@/services/db/loan-applications';
+import plaidClient from '@/utils/plaid';
+import { CountryCode, Products } from 'plaid';
 
-const validateRequest = async (args: {
-  loanApplicationId: string;
-  accessToken: string;
-  itemId: string;
-}) => {
-  const { loanApplicationId } = args;
-
-  const loanApplication = await dbGetLoanApplication({ loanApplicationId });
-
-  if (!loanApplication) {
-    throw new Error(`Loan application with id ${loanApplicationId} not found`);
-  }
-};
-
-interface SaveAccessTokenOfLoanApplicationCreatorResponse {
+interface GetLoanApplicationCreatorResponse {
   isError: boolean;
   errorMessage?: string;
+  account?: string;
 }
-export const saveAccessTokenOfLoanApplicationCreator = async (args: {
-  loanApplicationId: string;
-  accessToken: string;
-  itemId: string;
-}): Promise<SaveAccessTokenOfLoanApplicationCreatorResponse> => {
+export const getLoanApplicationCreator = async (
+  loanApplicationId: string
+): Promise<GetLoanApplicationCreatorResponse> => {
   try {
-    await validateRequest(args);
+    const loanApplication = await dbGetLoanApplication({ loanApplicationId });
 
-    await dbSaveAccessTokenOfLoanApplicationCreator(args);
+    if (!loanApplication) {
+      return {
+        isError: true,
+        errorMessage: `Loan application with id ${loanApplicationId} not found`,
+      };
+    }
 
     return {
       isError: false,
+      account: loanApplication.chainAccountAddress,
     };
   } catch (error) {
     return {
       isError: true,
-      errorMessage: 'Failed to save access token of loan application creator',
+      errorMessage: 'Failed to get loan application creator',
     };
   }
 };
+
+interface CreateLinkTokenResponse {
+  isError: boolean;
+  errorMessage?: string;
+  linkToken?: string;
+}
+export async function createLinkTokenForTransactions(
+  chainAccountAddress: string
+): Promise<CreateLinkTokenResponse> {
+  try {
+    const response = await plaidClient.linkTokenCreate({
+      client_id: process.env.PLAID_CLIENT_ID,
+      secret: process.env.PLAID_SECRET,
+      user: { client_user_id: chainAccountAddress },
+      products: [Products.Transactions],
+      transactions: {
+        days_requested: 730,
+      },
+      country_codes: [CountryCode.Us],
+      client_name: 'Locale Lending Platform',
+      language: 'en',
+      // redirect_uri: process.env.PLAID_REDIRECT_URI //TODO: reclaim success page
+    });
+
+    return {
+      isError: false,
+      linkToken: response.data.link_token,
+    };
+  } catch (error) {
+    console.log('error : ', error);
+    return {
+      isError: true,
+      errorMessage: 'Error creating link token',
+    };
+  }
+}
+
+interface PlaidPublicTokenExchangeResponse {
+  isError: boolean;
+  errorMessage?: string;
+  accessToken?: string;
+  itemId?: string;
+}
+export async function plaidPublicTokenExchange(publicToken: string): Promise<PlaidPublicTokenExchangeResponse> {
+  try {
+    
+    const response = await plaidClient.itemPublicTokenExchange({ public_token: publicToken });
+    const accessToken = response.data.access_token;
+    const itemId = response.data.item_id;
+    return {
+      isError: false,
+      accessToken,
+      itemId,
+    };
+  } catch (err) {
+    return {
+      isError: true,
+      errorMessage: 'Error exchanging public token for access token',
+    };
+  }
+}
