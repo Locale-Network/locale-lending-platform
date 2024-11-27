8bc70f7ba7799a02eed089c06271a2c6e9777f2c
sajee_techi <sajeewa@techi.be>
2024-11-27 19:11:02 +0530

    handle plaid identity verification outcome

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -3,3 +3,14 @@ import type { Address } from 'viem';
 export function formatAddress(address: Address) {
   return `${address.slice(0, 6)}...${address.slice(-4)}`;
 }
+
+export function snakeToSentenceCase(str: string): string {
+  return (
+    str
+      // Split by underscore and join with space
+      .split('_')
+      .join(' ')
+      // Capitalize first letter
+      .replace(/^\w/, c => c.toUpperCase())
+  );
+}
