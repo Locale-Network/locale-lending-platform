cc1417dcf2e76cc5c00b11a7b142f8b3c73bf01b
sajee_techi <sajeewa@techi.be>
2024-11-27 19:10:27 +0530

    deprecate

--- src/app/borrower/actions.tsx
+++ src/app/borrower/actions.tsx
@@ -1,11 +1,7 @@
 'use server';
 import { authOptions } from '@/app/api/auth/auth-options';
-import { getKycVerification } from '@/services/db/plaid/kyc';
-import { isPlaidError } from '@/types/plaid';
-import plaidClient from '@/utils/plaid';
 import { Role } from '@prisma/client';
 import { getServerSession } from 'next-auth';
-import { CountryCode, IdentityVerificationGetResponse, Products, Strategy } from 'plaid';
 import { isAddress } from 'viem';
 import { redirect } from 'next/navigation';
 import { ROLE_REDIRECTS } from '@/app/api/auth/auth-options';
@@ -27,78 +23,5 @@ export async function validateRequest(accountAddress: string) {
 
   if (!isAddress(accountAddress)) {
     throw new Error('Invalid chain account address');
-  }
-}
-
-interface GetKycStatusResponse {
-  isError: boolean;
-  errorMessage?: string;
-  hasAttemptedKyc: boolean;
-  identityVerificationData?: IdentityVerificationGetResponse;
-}
-
-export async function plaidKycStatus(identity_verification_id: string) {
-  return await plaidClient.identityVerificationGet({
-    identity_verification_id,
-  });
-}
-
-export async function getKycStatus(accountAddress: string): Promise<GetKycStatusResponse> {
-  try {
-    await validateRequest(accountAddress);
-    const kycVerification = await getKycVerification({ accountAddress });
-
-    if (!kycVerification) {
-      return { isError: false, hasAttemptedKyc: false };
-    }
-
-    const response = await plaidKycStatus(kycVerification.identityVerificationId);
-
-    return { isError: false, hasAttemptedKyc: true, identityVerificationData: response.data };
-  } catch (error: any) {
-    if (isPlaidError(error)) {
-      return { isError: true, hasAttemptedKyc: false, errorMessage: 'Error from PLAID' };
-    }
-    return { isError: true, hasAttemptedKyc: false, errorMessage: 'client error' };
-  }
-}
-
-export async function retryKyc(accountAddress: string) {
-  try {
-    await validateRequest(accountAddress);
-
-    const response = await plaidClient.identityVerificationRetry({
-      client_user_id: accountAddress,
-      template_id: process.env.TEMPLATE_ID || '',
-      strategy: Strategy.Reset,
-      secret: process.env.PLAID_SECRET,
-    });
-
-    return response.data;
-  } catch (error) {
-    console.error('Error retrying KYC:', error);
-    return null;
-  }
-}
-
-export async function createKycLinkToken(accountAddress: string) {
-  try {
-    await validateRequest(accountAddress);
-
-    const response = await plaidClient.linkTokenCreate({
-      user: { client_user_id: accountAddress },
-      products: [Products.IdentityVerification],
-      identity_verification: {
-        template_id: process.env.TEMPLATE_ID || '',
-      },
-      client_name: 'Locale Lending Platform',
-      language: 'en',
-      country_codes: [CountryCode.Us],
-    });
-
-    return response.data;
-  } catch (error) {
-    console.error('Error creating KYC link token:', error);
-    return null;
   }
 }
