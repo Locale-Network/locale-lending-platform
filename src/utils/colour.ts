1367d8ddf491d9aa2e72d4c0fdc4a74e2eaadca5
sajee_techi <sajeewa@techi.be>
2024-11-28 16:38:32 +0530

    util functions

--- src/utils/colour.ts
+++ src/utils/colour.ts
@@ -1,4 +1,5 @@
 import { LoanApplicationStatus } from '@prisma/client';
+import { normalizeCreditScore } from './number';
 
 export const getLoanStatusStyle = (status: LoanApplicationStatus) => {
   const statusStyles: Record<LoanApplicationStatus, string> = {
@@ -10,4 +11,14 @@ export const getLoanStatusStyle = (status: LoanApplicationStatus) => {
   };
 
   return statusStyles[status as keyof typeof statusStyles] || 'bg-gray-100 text-gray-800';
+};
+
+export const creditScoreProgressColor = (rangeMin: number, rangeMax: number, score: number) => {
+  const normalizedScore = normalizeCreditScore(rangeMin, rangeMax, score);
+
+  if (normalizedScore < 30) return 'bg-red-500'; // 0-30%
+  if (normalizedScore < 50) return 'bg-orange-500'; // 30-50%
+  if (normalizedScore < 70) return 'bg-yellow-500'; // 50-70%
+  if (normalizedScore < 90) return 'bg-green-500'; // 70-90%
+  return 'bg-blue-500'; // 90-100%
 };
