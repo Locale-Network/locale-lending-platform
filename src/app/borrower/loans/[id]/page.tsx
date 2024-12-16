527d6150f6dc56df8cd1ac853798e924ede2dfed
sajee_techi <sajeewa@techi.be>
2024-12-16 10:24:43 +0530

    borrower loan can connect new bank account via reclaim plaid flow

--- src/app/borrower/loans/[id]/page.tsx
+++ src/app/borrower/loans/[id]/page.tsx
@@ -1,9 +1,12 @@
 import { getLoanApplication } from './actions';
+import { initialiseReclaimPlaidProof } from '@/app/borrower/loans/apply/actions-reclaim';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/api/auth/auth-options';
 import BusinessInformation from './business-information';
 import LoanInformation from './loan-information';
 import OutstandingLoans from './outstanding-loans';
+import { LoanApplicationStatus } from '@prisma/client';
+import ReclaimPlaid from './reclaim-plaid';
 
 export default async function Page({ params }: { params: { id: string } }) {
   const { id } = params;
@@ -28,11 +31,29 @@ export default async function Page({ params }: { params: { id: string } }) {
     return <>loan with id {id} not found</>;
   }
 
+  const { requestUrl: reclaimPlaidRequestUrl, statusUrl: reclaimPlaidStatusUrl } =
+    await initialiseReclaimPlaidProof({
+      accountAddress,
+      loanApplicationId: id,
+    });
+
   return (
-    <div className="grid grid-cols-1 gap-4 p-4 md:grid-cols-2">
-      <LoanInformation loanApplication={loanApplication} />
-      <BusinessInformation business={loanApplication} />
-      <OutstandingLoans loans={loanApplication.outstandingLoans} />
-    </div>
+    <>
+      {loanApplication.status !== LoanApplicationStatus.APPROVED && (
+        <div className="flex justify-end">
+          <ReclaimPlaid
+            requestUrl={reclaimPlaidRequestUrl}
+            statusUrl={reclaimPlaidStatusUrl}
+            loanApplicationId={id}
+          />
+        </div>
+      )}
+      <div className="my-4" />
+      <div className="grid grid-cols-1 gap-4 p-0 md:grid-cols-2">
+        <LoanInformation loanApplication={loanApplication} />
+        <BusinessInformation business={loanApplication} />
+        <OutstandingLoans loans={loanApplication.outstandingLoans} />
+      </div>
+    </>
   );
 }
